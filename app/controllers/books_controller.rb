5879bd514d5850315c9732788d4fa1c2a79e5ee1
coma-dwc <125862959+tamagomapon@users.noreply.github.com>
2023-03-26 10:56:30 +0000

    [finish]応用課題2 RSpecテスト修正終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,44 +1,51 @@
 class BooksController < ApplicationController
-  before_action :is_matchig_login_user_book, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
-    @book = Book.find(params[:id])
-    @book_new = Book.new
-    @users = @book.user
+     @book = Book.find(params[:id])
+     @book_new = Book.new
+     @user = @book.user
   end
 
   def index
-    @books = Book.all
-    @book = Book.new
+     @books = Book.all
+     @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
+      @user = current_user
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
-  end
-
-  def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
-    else
+    if @book.user == current_user
       render "edit"
+    else
+      redirect_to books_path
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
+  def update
+     @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+     @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
   end
 
@@ -47,12 +54,11 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-  def is_matchig_login_user_book
-    book = Book.find(params[:id])
-    unless book.user.id == current_user.id
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    if @book.user != current_user
       redirect_to books_path
     end
   end
-  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:update, :edit]
 
   def show
@@ -10,6 +11,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @user = current_user
   end
 
   def edit
